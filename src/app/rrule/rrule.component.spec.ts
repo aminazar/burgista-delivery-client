b9e261b649faddacd34bb0d273cd16535355228a
Amin Azarbadegan <amin@azarbadegan.net>
2017-02-20 18:43:08 +0330

    Final touches on RRule component / resolving test failures because of newer version of ng2 material / adding userType to auth service

--- src/app/rrule/rrule.component.spec.ts
+++ src/app/rrule/rrule.component.spec.ts
@@ -3,19 +3,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {By, BrowserModule} from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { RruleComponent } from './rrule.component';
+import {RRuleComponent} from './rrule.component';
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {MaterialModule} from "@angular/material";
 import {RouterTestingModule} from "@angular/router/testing";
+import {MonthdayComponent} from "./monthday.component";
 
 describe('RruleComponent', () => {
-  let component: RruleComponent;
-  let fixture: ComponentFixture<RruleComponent>;
+  let component: RRuleComponent;
+  let fixture: ComponentFixture<RRuleComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RruleComponent ],
+      declarations: [ RRuleComponent, MonthdayComponent ],
       imports: [
         BrowserModule,
         FormsModule,
@@ -28,7 +29,7 @@ describe('RruleComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RruleComponent);
+    fixture = TestBed.createComponent(RRuleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
