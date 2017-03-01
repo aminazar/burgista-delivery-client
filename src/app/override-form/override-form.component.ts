281967c551bedd1a1691c89d6df29833162918e1
Alireza\Ali <ali.71hariri@gmail.com>
2017-03-01 12:26:18 +0330

    Check product name on add and update in product-form (avoid duplication on name) Improve navbar to show just related and allowed items to user (Admin/Branch)

--- src/app/override-form/override-form.component.ts
+++ src/app/override-form/override-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
 
 import {RestService} from "../rest.service";
